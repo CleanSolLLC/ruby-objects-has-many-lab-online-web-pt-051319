1b893959d997ee069f9245f686eb87e136b55769
Mark May <mamark90@gmail.com>
2019-06-16 22:26:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
     def add_song_by_name(song)
         song = Song.new(song)
         @songs << song
-        post.author = self
+        song.artist = self
     end
 
     def self.song_count
